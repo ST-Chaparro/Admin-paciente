6639d01f0af20c2ee22613928ce43b9608a89e26
Stiven <stiven4158bry@gmail.com>
2024-08-16 17:35:40 -0500

    alertas y guaradado de pacientes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <script setup>
+import {ref, reactive} from 'vue'
 import Header from './components/Header.vue';
 import Formulario from './components/Formulario.vue';
+
+const pacientes = ref([])
+
+const paciente = reactive({
+    nombre: '',
+    propietario: '',
+    email: '',
+    alta: '',
+    sintomas: ''
+})
+
+const guardarPaciente  = () => {
+  pacientes.value.push(paciente)
+}
+
 
 </script>
 
@@ -10,10 +26,26 @@ import Formulario from './components/Formulario.vue';
 
     <Header/>
 
-     <div class="mt-12 md:flex">
+      <div class="mt-12 md:flex">
       
-      <Formulario/>
-     </div>
+        <Formulario
+          v-model:nombre="paciente.nombre"
+          v-model:propietario="paciente.propietario"
+          v-model:email="paciente.email"
+          v-model:alta="paciente.alta"
+          v-model:sintomas="paciente.sintomas"
+          @guardar-paciente="guardarPaciente"
+        />
+
+        <div class="md:w-1/2 md:h-screen overflow-y-scroll" >
+          <h3 class="font-black text-3xl text-center ">Administra tus Pacientes</h3>
+
+            <div v-if="pacientes.length > 0">
+
+            </div>
+            <p v-else class="mt-10 text-2xl text-center">No Hay Pacientes</p>
+        </div>
+      </div>
   </div>
   
 </template>
